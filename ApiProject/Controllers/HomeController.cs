d4665448633c96f98811d9b2c39c8992538c537b
Hariharan.M <Hariharan.M@Krossark.local>
2020-06-20 21:58:14 +0530

    Commit

---
+++ ApiProject/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ApiProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace ApiProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration config)
+        {
+            configuration = config;
+        }
+
+        [HttpGet]
+        public List<MyDetails> Api()
+        {
+            MongoClient client = new MongoClient(configuration.GetConnectionString("DefaultConnection"));
+            IMongoDatabase database = client.GetDatabase("myDB");
+            IMongoCollection<MyDetails> myDetails = database.GetCollection<MyDetails>("myDetails");
+            MyDetails myFirstDetails = new MyDetails
+            {
+                _id = ObjectId.GenerateNewId(),
+                name = "Hari",
+                email = "harikrrish27@grr.la"
+            };
+
+            myDetails.InsertOne(myFirstDetails);
+
+            List<MyDetails> collection = myDetails.Find(new BsonDocument()).ToList();
+            //myDetails.UpdateOne(Builders<MyDetails>.Filter.Eq("_id", collection[0]._id), Builders <MyDetails>.Update.Set("name", "Done"));
+            return collection;
+        }
+    }
+}
---
+++ ApiProject/Models/MyDetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ApiProject.Models
+{
+    public class MyDetails
+    {
+        public Object _id {get; set;}
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+}
